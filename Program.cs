5d48507ecf1a7451ce03634956f0a35740166947
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2022-03-16 15:27:44 -0300

    Added the user creation and login

--- Program.cs
+++ Program.cs
@@ -3,10 +3,13 @@ using DemoMinimalAPI.AutoMapper;
 using DemoMinimalAPI.Data;
 using DemoMinimalAPI.DTOs;
 using DemoMinimalAPI.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using MiniValidation;
 using NetDevPack.Identity;
 using NetDevPack.Identity.Jwt;
+using NetDevPack.Identity.Model;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -35,6 +38,83 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthConfiguration();
 app.UseHttpsRedirection();
+
+
+app.MapPost("/register", async (
+    SignInManager<IdentityUser> signInManager,
+    UserManager<IdentityUser> userManager,
+    IOptions<AppJwtSettings> appJwtSettings,
+    RegisterUser registerUser) =>
+{
+    if (registerUser is null)
+        return Results.BadRequest("User not sent");
+
+    if (!MiniValidator.TryValidate(registerUser, out var errors))
+        return Results.ValidationProblem(errors);
+
+    var user = new IdentityUser
+    {
+        UserName = registerUser.Email,
+        Email = registerUser.Email,
+        EmailConfirmed = true
+    };
+
+    var result = await userManager.CreateAsync(user, registerUser.Password);
+    if (!result.Succeeded)
+        return Results.BadRequest(result.Errors);
+
+    var jwt = new JwtBuilder()
+                .WithUserManager(userManager)
+                .WithJwtSettings(appJwtSettings.Value)
+                .WithEmail(user.Email)
+                .WithJwtClaims()
+                .WithUserClaims()
+                .WithUserRoles()
+                .BuildUserResponse();
+
+    return Results.Ok(jwt);
+}).ProducesValidationProblem()
+  .Produces(StatusCodes.Status200OK)
+  .Produces(StatusCodes.Status400BadRequest)
+  .WithName("RegisterUser")
+  .WithTags("User");
+
+app.MapPost("/login", async (
+    SignInManager<IdentityUser> signInManager,
+    UserManager<IdentityUser> userManager,
+    IOptions<AppJwtSettings> appJwtSettings,
+    LoginUser loginUser) =>
+{
+    if (loginUser == null)
+        return Results.BadRequest("User not sent");
+
+    if (!MiniValidator.TryValidate(loginUser, out var errors))
+        return Results.ValidationProblem(errors);
+
+    var result = await signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+    if (result.IsLockedOut)
+        return Results.BadRequest("User blocked");
+
+    if (!result.Succeeded)
+        return Results.BadRequest("User or password invalid");
+
+    var jwt = new JwtBuilder()
+                .WithUserManager(userManager)
+                .WithJwtSettings(appJwtSettings.Value)
+                .WithEmail(loginUser.Email)
+                .WithJwtClaims()
+                .WithUserClaims()
+                .WithUserRoles()
+                .BuildUserResponse();
+
+    return Results.Ok(jwt);
+}).ProducesValidationProblem()
+  .Produces(StatusCodes.Status200OK)
+  .Produces(StatusCodes.Status400BadRequest)
+  .WithName("LoginUser")
+  .WithTags("User");
+
 
 app.MapGet("/suppliers", async (MinimalContextDb context) =>
     await context.Suppliers.ToListAsync())
